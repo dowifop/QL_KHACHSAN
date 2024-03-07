b9760b12eac17171431f29920a95a02e3a561908
dowifop <118067235+dowifop@users.noreply.github.com>
2024-03-08 00:37:50 +0700

    xoa

---
+++ QLHOMESTAY/TestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using QLKS;
+namespace TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
