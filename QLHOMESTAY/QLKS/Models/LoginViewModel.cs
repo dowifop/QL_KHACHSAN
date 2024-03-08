6639780b964bf74e7c0d846666f37b3f5099b67f
dowifop <118067235+dowifop@users.noreply.github.com>
2024-03-08 22:20:34 +0700

    login

---
+++ QLHOMESTAY/QLKS/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QLKS.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập Tên Đăng Nhập.")]
+        public string ma_kh { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Mật Khẩu.")]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải chứa ít nhất 6 kí tự.")]
+        public string mat_khau { get; set; }
+    }
+}
