cf1cc064d511aebeb63bfd752e94f795765ddba6
ducnam1802 <vuducnam180203@gmail.com>
2024-03-09 14:45:10 +0700

    testInfor

--- QLHOMESTAY/TestQLKS/obj/Debug/net8.0/TestQLKS.AssemblyInfo.cs
+++ QLHOMESTAY/TestQLKS/obj/Debug/net8.0/TestQLKS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestQLKS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6492cb008cc69bb0a1e0a6fb249359e46feead0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11aa0a11d9b7bb5e61a26f6f4a47b7c51ea9071")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestQLKS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestQLKS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
